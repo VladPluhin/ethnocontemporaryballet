8e22b44fe930202c5a5f4d9671deaef0324d33f2
VladPluhin <vlad.plugin@comprend.com>
2024-01-28 18:10:02 +0200

    build

--- src/components/socialList/socialList.js
+++ src/components/socialList/socialList.js
@@ -1,19 +1,18 @@
-import * as React from "react"
-import './socialLis.scss';
+import * as React from "react";
+import "./socialLis.scss";
 
-function SocialList({data, nameOfBlock}) {
-    return(
-        <ul className="social-list">
-             { data.map((icon)=> {
-                return (
-                    <li key={nameOfBlock + "-" + icon.nameIcon}>
-                        <a href={icon.iconLinkUrl} className={'icon-' + icon.nameIcon}></a>
-                    </li>
-                    )
-                })}
-        </ul>
-    )
+function SocialList({ data, nameOfBlock }) {
+  return (
+    <ul className="social-list">
+      {data.map((icon) => {
+        return (
+          <li key={nameOfBlock + "-" + icon.nameIcon}>
+            <a href={icon.iconLinkUrl} className={"icon-" + icon.nameIcon}></a>
+          </li>
+        );
+      })}
+    </ul>
+  );
 }
 
 export default SocialList;
-
