5b647e3f5f530df95600cde59a58c68f80f053fd
VladPluhin <vlad.plugin@comprend.com>
2024-01-28 19:58:57 +0200

    Update seo.js

--- src/components/seo.js
+++ src/components/seo.js
@@ -5,8 +5,8 @@
  * See: https://www.gatsbyjs.com/docs/how-to/querying-data/use-static-query/
  */
 
-import * as React from "react"
-import { useStaticQuery, graphql } from "gatsby"
+import * as React from "react";
+import { useStaticQuery, graphql } from "gatsby";
 
 function Seo({ description, title, children }) {
   const { site } = useStaticQuery(
@@ -22,10 +22,11 @@ function Seo({ description, title, children }) {
         }
       }
     `
-  )
+  );
 
-  const metaDescription = description || site.siteMetadata.description
-  const defaultTitle = site.siteMetadata?.title
+  const metaDescription = description || site.siteMetadata.description;
+  const defaultTitle = site.siteMetadata?.title;
+
   return (
     <>
       <title>{defaultTitle}</title>
@@ -39,7 +40,7 @@ function Seo({ description, title, children }) {
       <meta name="twitter:description" content={metaDescription} />
       {children}
     </>
-  )
+  );
 }
 
-export default Seo
+export default Seo;
