7f37deeb93564df780b7481cdd2a9017c7dc0e30
VladPluhin <vlad.plugin@comprend.com>
2024-08-04 14:08:25 +0300

    fixed

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,27 +1,29 @@
-import  React, {useEffect, useState} from "react"
+import React, { useEffect, useState } from "react";
 
-import Header from "./header/header"
-import Footer from "./footer/footer"
-import "./layout.scss"
+import Header from "./header/header";
+import Footer from "./footer/footer";
+import "./layout.scss";
 
 const Layout = ({ children }) => {
-  const [isActiveBurger, setIsActiveBurger] = useState(false)
- 
+  const [isActiveBurger, setIsActiveBurger] = useState(false);
+
   function initBurger() {
-    if(isActiveBurger) {
-      setIsActiveBurger(false)
-    }else{
-      setIsActiveBurger(true)
+    if (isActiveBurger) {
+      setIsActiveBurger(false);
+    } else {
+      setIsActiveBurger(true);
     }
   }
-  
   return (
-    <div className={isActiveBurger ? "wrapper burger-active" : 'wrapper' } id='wrapper' >
-        <Header initBurger={initBurger} />
-            <main>{children}</main>
-            <Footer/>           
+    <div
+      className={isActiveBurger ? "wrapper burger-active" : "wrapper"}
+      id="wrapper"
+    >
+      <Header initBurger={initBurger} />
+      <main>{children}</main>
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
